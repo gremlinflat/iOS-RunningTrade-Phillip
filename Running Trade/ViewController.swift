4321c383aee259c165aea30c52ecbcd94e829191
Gremlinflat <fahri.novaldi91100@gmail.com>
2022-10-04 14:42:29 +0700

    Initial Commit

---
+++ Running Trade/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Running Trade
+//
+//  Created by Fahri Novaldi on 04/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
